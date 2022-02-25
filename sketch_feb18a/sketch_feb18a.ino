fcd372af411b7a3dceb2cf2a8a4915661a09c792
David Natanael <b08611019@ntu.edu.tw>
2022-02-25 22:12:16 +0800

    function skeletons

--- sketch_feb18a/sketch_feb18a.ino
+++ sketch_feb18a/sketch_feb18a.ino
@@ -1,27 +1,45 @@
 #include <algorithm>
 
 const float kd = 1, ki = 1, kp = 1;
-struct Data {
-  float wx, wy, wz, ax, ay, az;
-};
+
+struct MPUData { float wx, wy, wz, ax, ay, az; };
 
 struct MPU {
-  Data data, new_data;
+  MPUData data, new_data;
 
-  Data read();
+  MPUData read();
 };
 
 MPU mpu;
 
 void setup() {
   // put your setup code here, to run once:
+
+  setPins();
   mpu.data = mpu.read();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
   mpu.new_data = mpu.read();
   calculate();
   run();
   mpu.data = mpu.new_data;
+}
+
+MPUData MPU::read() {
+
+}
+
+void calculate() {
+  
+}
+
+void run() {
+
+}
+
+void setPins() {
+
 }
