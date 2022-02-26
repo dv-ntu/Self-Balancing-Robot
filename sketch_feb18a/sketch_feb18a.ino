bb3135aeba9c1c746feeda1231a84919fe0cfd0e
David Natanael <b08611019@ntu.edu.tw>
2022-02-26 17:18:17 +0800

    tried to write pid

--- sketch_feb18a/sketch_feb18a.ino
+++ sketch_feb18a/sketch_feb18a.ino
@@ -5,27 +5,32 @@ const float kd = 1, ki = 1, kp = 1;
 struct MPUData { float wx, wy, wz, ax, ay, az; };
 
 struct MPU {
-  MPUData data, new_data;
+  MPUData old_data, data;
 
   MPUData read();
 };
 
 MPU mpu;
+MPUData e;
+
+void setPins();
+void calculate();
+void run();
 
 void setup() {
   // put your setup code here, to run once:
 
   setPins();
-  mpu.data = mpu.read();
+  mpu.old_data = mpu.read();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  mpu.new_data = mpu.read();
+  mpu.data = mpu.read();
   calculate();
   run();
-  mpu.data = mpu.new_data;
+  mpu.old_data = mpu.data;
 }
 
 MPUData MPU::read() {
@@ -33,7 +38,12 @@ MPUData MPU::read() {
 }
 
 void calculate() {
-  
+  float p, i, d;
+  p = mpu.data.ax;
+  // i = ...
+  d = mpu.data.ax - mpu.old_data.ax;
+
+  return kp * p + ki * i + kd * d;
 }
 
 void run() {
